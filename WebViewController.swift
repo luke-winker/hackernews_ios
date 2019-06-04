e5c6e33554088bfadd4edf40c7155411de636c5b
Luke Winker <luke.winker@cerner.com>
2019-06-04 15:01:31 -0500

    fixed comments bug

--- WebViewController.swift
+++ WebViewController.swift
@@ -35,14 +35,10 @@ class WebViewController: UIViewController {
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowCommentsSegue" {
+            let controller = segue.destination as! CommentsTableViewController
+            controller.item = targetItem
+        }
     }
-    */
-
 }
--- hackernews/CommentTableViewCell.swift
+++ hackernews/CommentTableViewCell.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class CommentTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var commentTextView: UITextView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var upvoteButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
